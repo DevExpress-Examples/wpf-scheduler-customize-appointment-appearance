a6286959e44e7a06228be74b85cfb37b521de0a7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomizeAppointmentExample/Converters/LabelToImageSourceConverter.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.Utils.Design;
+using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace CustomizeAppointmentExample
+{
+    #region #LabelToImageSourceConverter
+    public class LabelToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((int)value == 1)
+                return DXImageHelper.GetImageSource(@"Office2013\Tasks\Status_16x16.png");
+            else
+                return DXImageHelper.GetImageSource(@"Office2013\Support\Info_16x16.png");
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+    #endregion #LabelToImageSourceConverter
+
+    #region #LabelToImageSourceConverterExtension
+    public class LabelToImageSourceConverterExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new LabelToImageSourceConverter();
+        }
+    }
+    #endregion #LabelToImageSourceConverterExtension
+}
---
+++ VB/CustomizeAppointmentExample/Converters/LabelToImageSourceConverter.vb
@@ -0,0 +1,39 @@
+﻿Imports DevExpress.Utils.Design
+Imports DevExpress.Xpf.Core
+Imports System
+Imports System.Collections.Generic
+Imports System.Globalization
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Data
+Imports System.Windows.Markup
+Imports System.Windows.Media
+
+Namespace CustomizeAppointmentExample
+    #Region "#LabelToImageSourceConverter"
+    Public Class LabelToImageSourceConverter
+        Implements IValueConverter
+
+        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+            If DirectCast(value, Integer) = 1 Then
+                Return DXImageHelper.GetImageSource("Office2013\Tasks\Status_16x16.png")
+            Else
+                Return DXImageHelper.GetImageSource("Office2013\Support\Info_16x16.png")
+            End If
+        End Function
+        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Return value
+        End Function
+    End Class
+    #End Region ' #LabelToImageSourceConverter
+
+    #Region "#LabelToImageSourceConverterExtension"
+    Public Class LabelToImageSourceConverterExtension
+        Inherits MarkupExtension
+
+        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+            Return New LabelToImageSourceConverter()
+        End Function
+    End Class
+    #End Region ' #LabelToImageSourceConverterExtension
+End Namespace
