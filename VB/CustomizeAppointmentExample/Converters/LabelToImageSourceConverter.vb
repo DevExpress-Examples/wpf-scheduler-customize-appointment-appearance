5e82e8fb94c7d864855ba8a0d52c4c3dd334ffb3
DevExpressExampleBot <fakeEmail>
2022-01-11 13:22:23 +0300

    Source auto update [skip ci]

--- VB/CustomizeAppointmentExample/Converters/LabelToImageSourceConverter.vb
+++ VB/CustomizeAppointmentExample/Converters/LabelToImageSourceConverter.vb
@@ -1,34 +1,30 @@
-﻿Imports DevExpress.Utils.Design
 Imports DevExpress.Xpf.Core
 Imports System
-Imports System.Collections.Generic
 Imports System.Globalization
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Data
 Imports System.Windows.Markup
-Imports System.Windows.Media
 
 Namespace CustomizeAppointmentExample
-    #Region "#ConditionToImageSourceConverter"
+
+#Region "#ConditionToImageSourceConverter"
     Public Class ConditionToImageSourceConverter
         Implements IValueConverter
 
         Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
             If value IsNot Nothing Then
-                If DirectCast(value, Boolean) Then
-                    Return DXImageHelper.GetImageSource("Office2013\Actions\Show_16x16.png")
-                End If
+                If CBool(value) Then Return DXImageHelper.GetImageSource("Office2013\Actions\Show_16x16.png")
             End If
+
             Return Nothing
         End Function
+
         Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
             Return value
         End Function
     End Class
-    #End Region ' #ConditionToImageSourceConverter
 
-    #Region "#ConditionToImageSourceConverterExtension"
+#End Region  ' #ConditionToImageSourceConverter
+#Region "#ConditionToImageSourceConverterExtension"
     Public Class ConditionToImageSourceConverterExtension
         Inherits MarkupExtension
 
@@ -36,5 +32,5 @@ Namespace CustomizeAppointmentExample
             Return New ConditionToImageSourceConverter()
         End Function
     End Class
-    #End Region ' #ConditionToImageSourceConverterExtension
+#End Region  ' #ConditionToImageSourceConverterExtension
 End Namespace
