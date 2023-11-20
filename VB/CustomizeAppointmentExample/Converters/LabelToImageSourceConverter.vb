65abf0f5c48e4e01f0dea6d7db4451b2b8de9641
DevExpressExampleBot <fakeEmail>
2023-11-20 14:25:12 +0400

    Source auto update [skip ci]

--- VB/CustomizeAppointmentExample/Converters/LabelToImageSourceConverter.vb
+++ VB/CustomizeAppointmentExample/Converters/LabelToImageSourceConverter.vb
@@ -1,33 +1,30 @@
-﻿Imports DevExpress.Utils.Design
 Imports DevExpress.Xpf.Core
 Imports System
-Imports System.Collections.Generic
 Imports System.Globalization
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Data
 Imports System.Windows.Markup
-Imports System.Windows.Media
 
 Namespace CustomizeAppointmentExample
-    #Region "#LabelToImageSourceConverter"
+
+#Region "#LabelToImageSourceConverter"
     Public Class LabelToImageSourceConverter
         Implements IValueConverter
 
         Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
-            If DirectCast(value, Integer) = 1 Then
+            If CInt(value) = 1 Then
                 Return DXImageHelper.GetImageSource("Office2013\Tasks\Status_16x16.png")
             Else
                 Return DXImageHelper.GetImageSource("Office2013\Support\Info_16x16.png")
             End If
         End Function
+
         Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
             Return value
         End Function
     End Class
-    #End Region ' #LabelToImageSourceConverter
 
-    #Region "#LabelToImageSourceConverterExtension"
+#End Region  ' #LabelToImageSourceConverter
+#Region "#LabelToImageSourceConverterExtension"
     Public Class LabelToImageSourceConverterExtension
         Inherits MarkupExtension
 
@@ -35,5 +32,5 @@ Namespace CustomizeAppointmentExample
             Return New LabelToImageSourceConverter()
         End Function
     End Class
-    #End Region ' #LabelToImageSourceConverterExtension
+#End Region  ' #LabelToImageSourceConverterExtension
 End Namespace
